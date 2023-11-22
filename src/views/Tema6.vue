b7ea40ea482d87cd428cf194eb4b9eeb4505ccc8
Oscar Espitia <odespitia@sena.edu.co>
2023-11-21 21:43:11 -0500

    final tema 1 al 7

--- src/views/Tema6.vue
+++ src/views/Tema6.vue
@@ -5,7 +5,100 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 6
-      h1 Titulo de primer nivel
+      h1 Posicionamiento
+
+    p.mb-3(data-aos="fade-down") El posicionamiento es uno de los objetivos más deseados por las empresas, debido a que, si la empresa se posiciona en las redes sociales, esto se convierte en un sello que garantiza que sea conocida y genere confianza ante el consumidor. 
+    p.mb-5(data-aos="fade-down") Posicionar no es solo que aparezca en los buscadores más reconocidos en los primeros lugares, es hacer presencia en las demás redes sociales de manera orgánica y, si el presupuesto lo permite, también invirtiendo en los mejores espacios. 
+
+    .bloque-texto-g.color-primario.p-3.p-sm-4.p-md-5.mb-5(data-aos="fade-down")
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/temas/tema6/tema6-1.png')})`}"
+      )
+      .bloque-texto-g__texto.p-4
+        p.mb-0 Antes de posicionar una empresa o marca, es importante: tener registrada la marca de la empresa o que la PYME esté legalmente constituida. De esta manera, con total tranquilidad, se puede difundir la información, como sus logo-símbolos, productos y servicios. 
+
+    p.mb-3(data-aos="fade-down") Se necesita un buen posicionamiento en las redes sociales por la cantidad de plataformas que existen hoy en Internet; las redes sociales tienen un gran alcance y ofrecen muchas oportunidades para promocionar marcas o negocios.
+    p.mb-3(data-aos="fade-down") Entre las razones por las que se ha convertido en una necesidad posicionarse en redes sociales, están:
+
+    .tarjeta.color-acento-botones.p-4.mb-5(data-aos="fade-down")
+      .row.justify-content-center.align-items-center
+        .col-12.col-lg-8
+          ul.lista-ul.ms-3
+            li.mb-0
+              span.icon ✔ 
+              | Desarrollar la imagen y mejorarla constantemente (imagen corporativa).
+            li.mb-0
+              span.icon ✔ 
+              | Ganar audiencia y conseguir nuevos seguidores o clientes.
+            li.mb-0
+              span.icon ✔ 
+              | Comercializar los productos y servicios de forma más eficiente, ya que se focaliza el segmento destinatario.
+            li.mb-0
+              span.icon ✔ 
+              | Es más fácil fidelizar clientes porque se tiene un contacto inmediato y permanente, lo cual permite responder a preguntas, quejas y reclamos rápidamente.
+            li.mb-0
+              span.icon ✔ 
+              | Optimiza los motores de búsqueda SEO, lo que permite que el contenido se mantenga actualizado automáticamente y lo pone en una posición de liderazgo en #[em Google] y otros motores de búsqueda.
+            li.mb-0
+              span.icon ✔ 
+              | Mejorar las ventas de los productos o servicios, ya que se incrementa el efecto de comercialización y da vida a otros canales de #[em marketing], tales como #[em blogs], boletines de noticias, seminarios web, eventos #[em offline], lo cual permite un mejor aprovechamiento de las redes sociales.
+            li.mb-0
+              span.icon ✔ 
+              | Tener implementadas plataformas de pago virtual, para garantizar la facilidad de adquisición de los productos. 
+        .col-12.col-lg-3
+          img(src='@/assets/curso/temas/tema6/tema6-2.png', alt='')
+
+    p.mb-3(data-aos="fade-down") A la hora de seleccionar las plataformas de redes sociales más adecuadas, se debe ser muy cuidadoso, ya que se necesita buscar las que ofrecen las mejores oportunidades para atraer un grupo objetivo.
+    p.mb-5(data-aos="fade-down") A continuación, se relacionan una serie de páginas que prestan servicios para personalizar las redes sociales:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-lg-10(data-aos="fade-right")
+        .titulo-sexto.color-acento-contenido.mb-1
+          p.titulo.pb-0.mb-0 #[b Tabla 3.] #[i Páginas para personalizar] 
+        .tabla-a.color-acento-contenido
+          table(style="min-width:auto")
+            thead
+              tr.cabecera-a.text-center
+                th Nombre
+                th Objetivo
+            tbody
+              tr
+                td.text-center #[em Logaster]
+                td.text-start Permite la creación de logotipos para las plataformas de redes sociales.
+              tr
+                td.text-center #[em Crello.com]
+                td.text-start Permite diseñar imágenes para las portadas de sus sitios en redes sociales.
+              tr
+                td.text-center #[em Sproutsocial]
+                td.text-start Individualiza las imágenes con el ánimo de que se cumpla con las especificaciones solicitadas en las plataformas de redes sociales.
+              tr
+                td.text-center Pablo
+                td.text-start Permite la búsqueda de imágenes de portada y añade textos.
+              tr
+                td.text-center #[em Pixabay.com]
+                td.text-start Es un buscador de fotos de stock, de donde se pueden bajar excelentes fotos de portada gratuitamente.
+              tr
+                td.text-center #[em Pikwizard.com]
+                td.text-start Sitio de fotos de personas, usted puede bajarlas gratuitamente.
+              tr
+                td.text-center #[em Pexels.com]
+                td.text-start Imágenes gratuitas para crear portadas y contenido en redes sociales.
+              tr
+                td.text-center #[em Unsplash]
+                td.text-start Sitio web de fotos de stock que permite crear sitios únicos en redes sociales.
+              tr
+                td.text-center #[em Jaymantri.com]
+                td.text-start Se pueden crear imágenes de portada abstractas y bonitas publicaciones.
+              tr
+                td.text-center #[em Designspiration]
+                td.text-start Espacio extraordinario para buscar inspiración para contenidos a publicar en redes sociales.
+              tr
+                td.text-center #[em Fonts.google.com]
+                td.text-start Buscador de fuentes a consultar gratuito.
+              tr
+                td.text-center #[em Coolors.co]
+                td.text-start Extraordinarias representaciones de color para crear atractivas portadas.
+
 
 </template>
 
--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,7 +5,84 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 Titulo de primer nivel
+      h1 #[em Big Data]
+
+    .row.justify-content-center.align-items-center.mb-4(data-aos="fade-down")
+      .col-12.col-lg-10.mb-2
+        img(src='@/assets/curso/temas/tema7/tema7-1.png', alt='')
+      .col-12.col-lg-10 
+        .cajon.color-primario.p-4
+          p.mb-0 El #[em Big Data] es un proceso que examina e interpreta grandes volúmenes de datos, ya sea estructurados o no estructurado; son tan grandes y complejos que estos datos almacenados remotamente se vuelven difíciles de procesar con métodos tradicionales, sin embargo, son necesarios para que puedan ser utilizados por las empresas de todos los sectores económicos para la toma de decisiones.
+
+    p.mb-3(data-aos="fade-down") Hablar de Big Data es referirse a un conjunto de datos o combinaciones de conjuntos de datos de gran volumen y complejidad; la complejidad y velocidad con la que crecen dificulta muchas veces su captura, gestión o análisis, para lo cual se usan herramientas como bases de datos y estadísticas convencionales, teniendo en cuenta el tiempo para el que son útiles.
+    p.mb-3(data-aos="fade-down") El Big Data ha tomado gran importancia en los planes de comunicación digital debido a que, gracias a él, se puede implementar la analítica de datos, donde es el insumo número uno para las empresas, para conocer datos e información para llegar al público objetivo.
+    p.mb-5(data-aos="fade-down") A continuación, se pueden observar las principales características del Big Data:
+
+    .row.justify-content-center.align-items-center.mb-5
+      .col-12.col-lg-10(data-aos="fade-right")
+        .titulo-sexto.color-acento-contenido.mb-1
+          p.titulo.pb-0.mb-0 #[b Tabla 4.] #[i Características del Big Data] 
+        .tabla-a.color-acento-contenido
+          table(style="min-width:auto")
+            thead
+              tr.cabecera-a.text-center
+                th Nombre
+                th Objetivo
+            tbody
+              tr
+                td.text-center Su relevancia
+                td.text-start Porque para las empresas proporciona respuestas a preguntas que ni siquiera ellas mismas sabían que tenían, dicho de otra manera, facilita una gran cantidad de información que puede ser procesada o probada de la forma en que la empresa considere apropiada. 
+              tr
+                td.text-center Su función
+                td.text-start Los datos que se extraen y analizan contribuyen a conocer cómo se comportan los clientes, y permite la actualización y optimización de los inventarios con respecto a la demanda en tiempo real.
+              tr
+                td.text-center El análisis
+                td.text-start Ayuda a las empresas con el aprovechamiento de los datos y orienta a utilizarlos con el fin de que identifiquen nuevas oportunidades, las lleva a encontrar negocios inteligentes, igualmente, que hagan operaciones eficientes y brinden mayores garantías a sus clientes.
+
+    .tarjeta.color-acento-botones.p-4.mb-5(data-aos="fade-down")
+      .row.justify-content-center.align-items-center
+        .col-12.col-lg-6
+          p Las empresas exitosas que usan #[em Big Data] logran valor de la siguiente manera:
+          ul.lista-ul.ms-3
+            li.mb-0
+              span.icon ✔ 
+              | Reducen costos, ya que utilizan importantes tecnologías de datos, tales como #[em Hadoop] y el análisis basado en la nube, los costos disminuyen cuando se almacenan grandes cantidades de datos y contribuyen con la identificación de nuevas maneras de hacer negocios.
+            li.mb-0
+              span.icon ✔ 
+              | Logran tomar mejores decisiones rápidamente, ya que, con la velocidad de #[em Hadoop] y su análisis de memoria combinada, se logran analizar nuevas fuentes de consultas y la empresa puede analizar información inmediatamente y tomar decisiones con base en los datos obtenidos.
+            li.mb-0
+              span.icon ✔ 
+              | Sacar al mercado nuevos productos y servicios, ya que tienen la capacidad de identificar las necesidades del cliente y pueden darle lo que necesita; con el análisis de #[em Big Data] las empresas crean nuevos productos para satisfacer la necesidad del cliente.
+        .col-10.col-lg-4
+          img(src='@/assets/curso/temas/tema7/tema7-2.png', alt='')
+            
+    p.mb-5(data-aos="fade-down") Los tipos de datos son:
+
+    .row.mb-5(data-aos="zoom-in-down")
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0.d-flex.align-items-stretch
+        .tarjeta--boton.color-primario.p-4
+          .row.justify-content-center.mb-3
+            .col-7.col-lg-6
+              figure
+                img(src='@/assets/curso/temas/tema7/tema7-5.svg', alt='') 
+          h4.text-center Datos no estructurados
+          p En ellos, encontramos: documentos, videos, audios, etc.
+      .col-sm-6.col-lg-4.mb-4.mb-lg-0.d-flex.align-items-stretch
+        .tarjeta--boton.color-primario.p-4
+          .row.justify-content-center.mb-3
+            .col-7.col-lg-6
+              figure
+                img(src='@/assets/curso/temas/tema7/tema7-6.svg', alt='')
+          h4.text-center Datos semiestructurados
+          p Como, por ejemplo: #[em software], hojas de cálculo, informes.
+      .col-sm-6.col-lg-4.mb-4.mb-sm-0.d-flex.align-items-stretch
+        .tarjeta--boton.color-primario.p-4.w-100
+          .row.justify-content-center.mb-3
+            .col-7.col-lg-6
+              figure
+                img(src='@/assets/curso/temas/tema7/tema7-7.svg', alt='')
+          h4.text-center Datos estructurados
+          p Se encuentran: tabla Excel, hojas de cálculo, bases de datos y cuestionarios tipo #[em test].
 
 </template>
 
